57894149427f0e8abad2ee9c2411949325f9ba00
unknown <n_zarina@hotmail.com>
2024-02-28 14:32:46 +0300

    Add lesson and HW 4

--- 4_HW.py
+++ 4_HW.py
@@ -1,94 +1,88 @@
 # 4.1. Напишите функцию square, принимающую 1 аргумент — сторону квадрата, и возвращающую 3 значения
 # (с помощью кортежа):
 #      периметр квадрата, площадь квадрата и диагональ квадрата.
-# import math
-#
-# def square(a):
-#     return a*4, a**2, a*(math.sqrt(2))
+import math
 
-# print(square(4))
-#
+
+def square(a):
+    return a * 4, a ** 2, a * (math.sqrt(2))
+
+
+print(square(4))
+
+
 # 4.2. Напишите фукнцию, которая принимает произвольное количество именнованных аргументов и выводит их построчно
 #      в формате аргумент: значение. Например:
 # 	name: John
 # 	last_name: Smith
 # 	age: 35
 # 	position: web developer
-
-# def person(**kwargs):
-#     for key, value in kwargs.items():
-#         print(f'{key}: {value}')
-#
-# person(name='John', last_name='Smith', age=35, position='web developer')
+def person(**kwargs):
+    for key, value in kwargs.items():
+        print(f'{key}: {value}')
 
 
+person(name='John', last_name='Smith', age=35, position='web developer')
 
-# 4.3. Используя лямбда-выражение, из списка my_list = [20, -3, 15, 2, -1, -21] создайте новый список, содержащий только
-#      положительные числа
-# my_list = [20, -3, 15, 2, -1, -21]
-# filtered_list = list(filter(lambda item: item > 0, my_list))
-#
-# print(filtered_list)
+# 4.3. Используя лямбда-выражение, из списка my_list = [20, -3, 15, 2, -1, -21]
+#      создайте новый список, содержащий только положительные числа
+my_list = [20, -3, 15, 2, -1, -21]
+filtered_list = list(filter(lambda item: item > 0, my_list))
+print(filtered_list)
 
 # 4.4. Используя лямбда выражение, получите результат перемножения значений в предыдущем списке
-# from functools import reduce
-# new_list = reduce(lambda num1, num2: num1*num2, my_list)
-# print(new_list)
-#
-# new_list = reduce(lambda num1, num2: num1*num2, filtered_list)
-# print(new_list)
-# print(reduce(lambda x, y: x*y, [x for x in my_list if x > 0])) #вариант преподавателя
+from functools import reduce
+
+new_list = reduce(lambda num1, num2: num1 * num2, my_list)
+print(new_list)
+
+# Чтобы получить результат перемножения только положительных значений
+# вариант 1
+new_list = reduce(lambda num1, num2: num1 * num2, filtered_list)
+print(new_list)
+# вариант 2 (вариант преподавателя)
+print(reduce(lambda x, y: x * y, [x for x in my_list if x > 0]))
 
 # 4.5. Напишите декоратор, который высчитывает время работы функции, которую он принимает в качестве параметра
-
 import time
 
-# def count_execution_time(func):
-#     def wrapper(*args):
-#         start = time.perf_counter()
-#         result = func(*args)
-#         end = time.perf_counter()
-#         exec_time = end - start
-#         print(f'{func.__name__} execution time is: {exec_time}')
-#         return result
-#     return wrapper
-#
-# @count_execution_time
-# def greeting(name):
-#     return f'Hello {name}!'
-#
-# greeting('Zarina')
-# print(greeting('Zarina'))
+
+def calculate_time_decorator(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print(f'The {func.__name__} function took {end - start} sec to execute')
+
+    return wrapper
 
 
-import time
-
-def greeting(name):
-    return f'Hello {name}!'
-
-
-def execution_time(time):
-    start = time.perf_counter()
-    greeting('Zarina')
-    end = time.perf_counter()
-    result = end - start
-    print(result)
-    return result
+@calculate_time_decorator
+def add(a, b):
+    time.sleep(2)
+    print(a + b)
 
 
+add(2, 3)
 
-print(greeting('Zarina'))
+print('-' * 50)
 
 
+@calculate_time_decorator
+def say_hello(name):
+    time.sleep(1)
+    print(f'Hello, {name}')
 
-# 4.6. Создайте файл my_calc.py и пропишите в нем минимум 4 функции, выполняющие базовые арифметические вычисления.
+
+say_hello('Zarina')
+
+# 4.6. Создайте файл my_calc.py и пропишите в нем минимум 4 функции, выполняющие базовые арифметические
+#      вычисления.
 #      Примените эти функции в качестве методов в другом файле.
 
-# import my_calc
-# print(f'Addition result is: {my_calc.addition(5,6)}')
-# print(my_calc.deduction(10, 5))
-# print(my_calc.multiplication(5,5))
-# print(my_calc.division(25, 10))
+import my_calc
 
-
-
+print(my_calc.add(5, 6))
+print(my_calc.deduce (10, 5))
+print(my_calc.multiply(5, 5))
+print(my_calc.divide(25, 10))
--- 4_lesson.py
+++ 4_lesson.py
@@ -1,59 +1,76 @@
-# def calc(a, b):
-#     return a * b
-#
-# print(calc(3, 4))
-#
-#
-# def calc(a, b):
-#     return a * b
-#
-# print(calc(b=5, a=2))
-#
-#
-# def calc(a, b):
-#     return a * b
-#
-# print(calc(b=5, a=2))
-#
-# def person(age, f_name, l_name='Niyazova'):
-# # def person(age, f_name, l_name):
-#     return f'Hello, my name is {f_name} {l_name}. I am {age} y.o.'
-#
-# print(person(33, 'Zarina', 'Niyazova'))
-# print(person(f_name='Zarina', l_name='Niyazova', age=33))
+def calc(a, b):
+    return a * b
+print(calc(3, 4)) # 12. Позиционные параметры
 
-# print(sum([5, 6, 7])) #сумма чисел
-# print(min([5, 6, 7]))
-# print(max([5, 6, 7]))
 
-# def pattern(length, char1, char2):
-#     return (char1 + char2) * length + char1
-#
-# print(pattern(8, '*', '-')) #*-*-*-*-*-*-*-*-*
-#
-#
-# def pattern(length, char1, char2='-'):
-#     return (char1 + char2) * length + char1
-#
-# print(pattern(8, '*')) #*-*-*-*-*-*-*-*-*
+def calc(a, b):
+    return a * b
+print(calc(b=5, a=2))  # 10. Именованные параметры
 
-# mult = lambda x, y: x/y
-# print(mult(5, 6))
+
+def calc(a, b, c=5):
+    return a + b + c
+print(calc(5, 4, c=10)) # 19, перепишет с на c = 10
+
+
+def args_function(a, b, *args): # *args - произвольное кол-во позиционных аргументов, вместо args может быть любое слово
+    print(a, b, args)
+args_function(1, 2, 1, 2, 3) # 1 2 (1, 2, 3)
+
+
+def kwargs_function(a, b, **kwargs): # **kwargs - произвольное кол-во именованных аргументов, вместо args может быть любое слово
+    print(a, b, kwargs)
+kwargs_function(1, 2, c=1, d=10) # 1 2 {'c': 1, 'd': 10}
+
+
+def person(age, f_name, l_name):
+    return f'Hello, my name is {f_name} {l_name}. I am {age} y.o.'
+print(person(f_name='Zarina', l_name='Niyazova', age=33))
+# можем передать аргументы в произвольном порядке, если укажем параметр
+
+
+def pattern(length, char1, char2):
+    return (char1 + char2) * length + char1
+print(pattern(8, '*', '-')) # *-*-*-*-*-*-*-*-*
+
+
+def pattern(length, char1, char2='-'):
+    return (char1 + char2) * length + char1
+print(pattern(8, '*')) # *-*-*-*-*-*-*-*-*
+
+
+# SPACE. ПРОСТРАНСТВО ИМЕН
+n = 10 # глобальное пространство имен
+def mother():
+    n = 20 # не локальное пространство имен
+    def son():
+        n = 30 # локальное пространство имен
+        print(n)
+    return son
+result = mother()
+result()
+# выведет 30, если не будет n = 30, то выведет 20, если не будет n = 20, то выведет 10
+
+
+# ЛЯМБДА-ФУНКЦИИ
+
+mult = lambda x, y: x/y
+print(mult(5, 6))
 
 l1 = [20, 15, 8, 7, 6]
 l = [20, 'str', 15, 18, 'yes', 'apple', 48, 40.5]
-# filtered = list(filter(lambda x: isinstance(x, int) and x % 2 == 0, l))
-# print(filtered) # -> [20, 18, 48]
-# print(*filtered) # * - оператор распаковки, распаковывает без скобок -> 20, 18, 48
-# filtered = list(filter(lambda x: not isinstance(x, str), l))
-# print(*filtered) # -> 20 15 18 48 40.5
-#
-# power = list(map(lambda x: x**2, l1))
-# print(power)
+filtered = list(filter(lambda x: isinstance(x, int) and x % 2 == 0, l))
+print(filtered) # -> [20, 18, 48]
+print(*filtered) # * - оператор распаковки, распаковывает без скобок -> 20, 18, 48
+filtered = list(filter(lambda x: not isinstance(x, str), l))
+print(*filtered) # -> 20 15 18 48 40.5
 
-# from functools import reduce
-# result = reduce(lambda x, y: x - y, l1)
-# print(result)
+power = list(map(lambda x: x**2, l1))
+print(power)
+
+from functools import reduce
+result = reduce(lambda x, y: x - y, l1)
+print(result)
 
 def my_decorator(func):
     def wrapper():
@@ -78,5 +95,3 @@ def my_decorator(func):
 @my_decorator
 def say_whee():
     print("Whee!")
-
-
